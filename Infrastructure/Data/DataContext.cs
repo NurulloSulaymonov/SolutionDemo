8dfd110d28ba4abc5611ffb5597a78cf5b49df19
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2023-01-16 19:18:59 +0500

    init

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class DataContext:DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options):base(options)
+    {
+        
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
---
+++ Infrastructure/Services/BookService.cs
@@ -0,0 +1,16 @@
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services;
+
+public class BookService:GenericCrud<Book>
+{
+    public BookService(DataContext context) : base(context)
+    {
+    }
+    
+    
+
+   
+}
